3d9f610e53a20beffd24a0c21a2ca656e751e629
Jon Palmisciano <jp@jonpalmisc.com>
2020-07-23 13:54:03 -0400

    feat: output now placed in family folder by default

--- vfit/core.py
+++ vfit/core.py
@@ -12,9 +12,11 @@ def generateInstances(config, args):
     metadata = config["metadata"]
     familyName = sanitize(metadata["family"])
 
+    outputDir = os.path.join(args.outputPath, familyName)
+
     # Create the output path if it doesn't exist.
-    if not os.path.exists(args.outputPath):
-        os.makedirs(args.outputPath)
+    if not os.path.exists(outputDir):
+        os.makedirs(outputDir)
 
     for style in tqdm(config["styles"], ascii=True, leave=False):
         font = TTFont(args.source)
@@ -26,6 +28,6 @@ def generateInstances(config, args):
         subfamilyName = style.get("subfamily") if "subfamily" in style else ""
 
         filename = f"{familyName}{sanitize(subfamilyName)}-{styleName}.ttf"
-        outputPath = os.path.join(args.outputPath, filename)
+        outputPath = os.path.join(outputDir, filename)
 
         font.save(outputPath)
