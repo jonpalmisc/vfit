983c9e7f02854739f09a70732d441a02b0d44c71
Jon Palmisciano <jp@jonpalmisc.com>
2020-12-29 11:18:21 -0500

    feat: added final FontForge bless step

--- vfit/core.py
+++ vfit/core.py
@@ -4,6 +4,8 @@ from fontTools.ttLib import TTFont
 from fontTools.varLib.instancer import instantiateVariableFont as instantiateFont
 from tqdm import tqdm
 
+import fontforge
+
 from .util import updateMetadata, makeSelection, getMacStyle, sanitize
 
 
@@ -13,6 +15,8 @@ def generateInstances(config, args):
     # Create the output path if it doesn't exist.
     if not os.path.exists(args.outputPath):
         os.makedirs(args.outputPath)
+
+    tempPaths = []
 
     for style in tqdm(config, ascii=True, leave=False):
         font = TTFont(args.source)
@@ -36,8 +40,18 @@ def generateInstances(config, args):
         font["head"].macStyle = getMacStyle(subfamilyName)
 
         ext = args.format if args.format is not None else "ttf"
-        filename = f"{familyName}-{prefSubfamily}.{ext}"
+        filename = f"{familyName}-{prefSubfamily}.{ext}.tmp"
         outputPath = os.path.join(args.outputPath, filename)
 
         font.flavor = args.format
         font.save(outputPath)
+
+        tempPaths.append(outputPath)
+
+    # Something about this makes the fonts work properly on Windows. I tried to
+    # figure this out for days. Font formats are cursed technology.
+    for path in tempPaths:
+        f = fontforge.open(path)
+        f.generate(path.replace(".tmp", ""))
+
+        os.unlink(path)
